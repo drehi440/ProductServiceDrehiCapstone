91d1f011bf073fe32d154d954285e8bd91e1d34a
deepesh.rehi <tl542nn6ieco2mxms3ush5limwivgzkhx4ginjxgwbk6mtvkrxyq>
2025-12-03 13:16:42 +0530

    Adding service methods implemenmtation for local DB

--- src/main/java/com/rehi/productservicedrehicapstone/services/ProductDBService.java
+++ src/main/java/com/rehi/productservicedrehicapstone/services/ProductDBService.java
@@ -2,34 +2,98 @@ package com.rehi.productservicedrehicapstone.services;
 
 
 import com.rehi.productservicedrehicapstone.exceptions.ProductNotFoundException;
+import com.rehi.productservicedrehicapstone.models.Category;
 import com.rehi.productservicedrehicapstone.models.Product;
+import com.rehi.productservicedrehicapstone.repositories.CategoryRepository;
+import com.rehi.productservicedrehicapstone.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service("productDBService")
 public class ProductDBService implements ProductService
 {
 
-    @Override
-    public Product getProductById(long id) throws ProductNotFoundException {
-        return null;
+    ProductRepository productRepository;
+    CategoryRepository categoryRepository;
+
+    public ProductDBService(ProductRepository productRepository,
+                            CategoryRepository categoryRepository)
+    {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
-    public List<Product> getAllProducts() {
-        return List.of();
+    public Product getProductById(long id) throws ProductNotFoundException
+    {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+
+        if(optionalProduct.isEmpty())
+        {
+            throw new ProductNotFoundException("Product with id " + id + " not found");
+        }
+
+        return optionalProduct.get();
     }
 
     @Override
-    public Product createProduct(String name, String description, double price, String imageUrl, String category) {
-        return null;
+    public List<Product> getAllProducts()
+    {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product createProduct(String name, String description, double price,
+                                 String imageUrl, String category)
+    {
+        Product product = new Product();
+        product.setName(name);
+        product.setDescription(description);
+        product.setPrice(price);
+        product.setImageUrl(imageUrl);
+
+        Category categoryObj = getCategoryFromDB(category);
+
+        product.setCategory(categoryObj);
+        return productRepository.save(product);
     }
 
     @Override
     public Product updateProduct(long id, String name, String description, double price,
                                  String imageUrl, String category) throws ProductNotFoundException
     {
-        throw new UnsupportedOperationException("updateProduct is not implemented for ProductDBService");
+        Optional<Product> optionalProduct = productRepository.findById(id);
+
+        if(optionalProduct.isEmpty())
+        {
+            throw new ProductNotFoundException("Product with id " + id + " not found");
+        }
+
+        Product product = optionalProduct.get();
+        product.setName(name);
+        product.setDescription(description);
+        product.setPrice(price);
+        product.setImageUrl(imageUrl);
+
+        Category categoryObj = getCategoryFromDB(category);
+        product.setCategory(categoryObj);
+
+        return productRepository.save(product);
+    }
+
+    private Category getCategoryFromDB(String name)
+    {
+        Optional<Category> optionalCategory = categoryRepository.findByName(name);
+        if(optionalCategory.isPresent())
+        {
+            return optionalCategory.get();
+        }
+
+        Category category = new Category();
+        category.setName(name);
+
+        return categoryRepository.save(category);
     }
 }
